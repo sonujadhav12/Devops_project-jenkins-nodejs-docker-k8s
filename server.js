70760c3deeef81ae63d3dfb71c71cbb722921c48
sonu jadhav <sonunidvanche3254@gmail.com>
2025-02-24 01:15:54 +0530

    final change

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello DevOps Cloud World. Thanks!!! ');
+  res.send('Hello DevOps Cloud World ,final change to my project.Thanks!!! ');
 });
 
 app.listen(PORT, HOST);
